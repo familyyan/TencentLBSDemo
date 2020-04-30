3ad15f1d0cb3262c8c1425f074e0ed79cfbe69aa
familyyan <wcystart0706@163.com>
2020-04-30 11:57:08 +0800

    update textview content

--- app/src/main/java/com/tencent/mapsdk/vector/tencentdemo/MainActivity.java
+++ app/src/main/java/com/tencent/mapsdk/vector/tencentdemo/MainActivity.java
@@ -3,12 +3,16 @@ package com.tencent.mapsdk.vector.tencentdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setText("TencentDemo");
     }
 }
